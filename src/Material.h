89d1ec8ff4b9996d30e3b8a834fc013d3eee9956
thirtytwo <58972099+thirtytytwo@users.noreply.github.com>
2024-09-28 16:28:22 +0800

    帧缓冲

--- src/Material.h
+++ src/Material.h
@@ -22,22 +22,24 @@ private:
 inline Material::Material(vector<Texture> textures, string shaderName):shader(Shader(std::move(shaderName)))
 {
     this->textures = std::move(textures);
-
+    shader.use();
     //TODO:后续加入别的贴图类别
-    this->textures[0].Active(GL_TEXTURE0);
     shader.SetInt("_MainTex", 0);
-    this->textures[0].Bind();
-    this->textures[1].Active(GL_TEXTURE1);
     shader.SetInt("_NormalTex", 1);
-    this->textures[1].Bind();
 }
 
 inline void Material::Render(Camera  &camera, Light &light, mat4 worldMatrix)
 {
+    //图片是要每帧都传的，有时间再弄清楚这几个关键字之前跟图片的区别
+    //如果不是每帧传，在GL_TEXTURE2D这个target中可能会出现有些图片每帧传然后把其他的图片覆盖掉，都改成了最新的绑定图片地址
     shader.use();
     shader.SetWolrd(worldMatrix);
     shader.SetCameraProps(camera);
     shader.SetLight(light);
+    this->textures[0].Active(GL_TEXTURE0);
+    this->textures[0].Bind();
+    this->textures[1].Active(GL_TEXTURE1);
+    this->textures[1].Bind();
 }
 
 
--- src/main.cpp
+++ src/main.cpp
@@ -31,24 +31,92 @@ int main()
         return -1;
     }
     
-	glEnable(GL_DEPTH_TEST);
 	Camera camera = Camera();
     Light light = Light();
     GameObject testObject = GameObject(vec3(0.f), vec3(0.0f), vec3(0.5f), "nanosuit");
 	camera.SetPosition(glm::vec3(0.0f, 5.0f, 3.0f));
 
+    float quadVertices[] = { // vertex attributes for a quad that fills the entire screen in Normalized Device Coordinates.
+        // positions   // texCoords
+        -1.0f,  1.0f,  0.0f, 1.0f,
+        -1.0f, -1.0f,  0.0f, 0.0f,
+         1.0f, -1.0f,  1.0f, 0.0f,
+
+        -1.0f,  1.0f,  0.0f, 1.0f,
+         1.0f, -1.0f,  1.0f, 0.0f,
+         1.0f,  1.0f,  1.0f, 1.0f
+    };
+    //Framebuffer 章节
+    // screen quad VAO
+    unsigned int quadVAO, quadVBO;
+    glGenVertexArrays(1, &quadVAO);
+    glGenBuffers(1, &quadVBO);
+    glBindVertexArray(quadVAO);
+    glBindBuffer(GL_ARRAY_BUFFER, quadVBO);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(quadVertices), &quadVertices, GL_STATIC_DRAW);
+    glEnableVertexAttribArray(0);
+    glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, 4 * sizeof(float), (void*)0);
+    glEnableVertexAttribArray(1);
+    glVertexAttribPointer(1, 2, GL_FLOAT, GL_FALSE, 4 * sizeof(float), (void*)(2 * sizeof(float)));
     
+    Shader shader = Shader("ResolveToScreen");
+    shader.use();
+    shader.SetInt("screenTexture", 0);
     
+    unsigned int framebuffer;
+    glGenFramebuffers(1, &framebuffer);
+    glBindFramebuffer(GL_FRAMEBUFFER, framebuffer);
+
+
+    //在这里 color 和 depth 是不同的格式去绑定，color是texture depth 是renderbuffer，应该是color可以后续我们用于采样，depth不需要所以用renderbuffer(猜测)
+    unsigned int colorAttachment;
+    glGenTextures(1, &colorAttachment);
+    glBindTexture(GL_TEXTURE_2D, colorAttachment);
+    glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, 800, 600, 0, GL_RGB, GL_UNSIGNED_BYTE, NULL);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+    glFramebufferTexture2D(GL_FRAMEBUFFER, GL_COLOR_ATTACHMENT0, GL_TEXTURE_2D, colorAttachment, 0);
+
+    unsigned int depthAttachment;
+    glGenRenderbuffers(1, &depthAttachment);
+    glBindRenderbuffer(GL_RENDERBUFFER, depthAttachment);
+    glRenderbufferStorage(GL_RENDERBUFFER, GL_DEPTH24_STENCIL8, 800, 600);
+    glFramebufferRenderbuffer(GL_FRAMEBUFFER, GL_DEPTH_STENCIL_ATTACHMENT, GL_RENDERBUFFER, depthAttachment);
+
+    if(glCheckFramebufferStatus(GL_FRAMEBUFFER) != GL_FRAMEBUFFER_COMPLETE)
+    {
+        std::cout << "Framebuffer is not complete" << std::endl;
+    }
+    glBindFramebuffer(GL_FRAMEBUFFER, 0);
     while (!glfwWindowShouldClose(window))
     {
+        glBindFramebuffer(GL_FRAMEBUFFER, framebuffer);
+        glEnable(GL_DEPTH_TEST);
+        glEnable(GL_CULL_FACE);
+        glCullFace(GL_BACK);
         glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
         glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
 		testObject.Render(camera, light);
+
+        //物体已经渲染到指定的framebuffer上，现在需要把framebuffer中的值拷贝到最终的屏幕上
+        glBindFramebuffer(GL_FRAMEBUFFER, 0);
+        glDisable(GL_DEPTH_TEST); // disable depth test so screen-space quad isn't discarded due to depth test.
+        // clear all relevant buffers
+        glClearColor(1.0f, 1.0f, 1.0f, 1.0f); // set clear color to white (not really necessary actually, since we won't be able to see behind the quad anyways)
+        glClear(GL_COLOR_BUFFER_BIT);
+
+        shader.use();
+        glBindVertexArray(quadVAO);
+        glBindTexture(GL_TEXTURE_2D, colorAttachment);	// use the color attachment texture as the texture of the quad plane
+        glDrawArrays(GL_TRIANGLES, 0, 6);
         glfwSwapBuffers(window);
         glfwPollEvents();
     }
-    
+    glDeleteVertexArrays(1, &quadVAO);
+    glDeleteBuffers(1, &quadVBO);
+    glDeleteRenderbuffers(1, &depthAttachment);
+    glDeleteFramebuffers(1, &framebuffer);
     glfwTerminate();
     return 0;
 }
